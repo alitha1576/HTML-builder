dbe3b08e645f56eef61083700648dfb364a85517
Anna Petrova <ann.undine@gmail.com>
2025-01-15 12:06:15 +0100

    finish 1 task

---
+++ 01-read-file/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('node:path');
+
+const filePath = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(filePath);
+
+readStream.on('data', function (chunk) {
+  console.log(chunk.toString());
+});
+
+// readStream.on('end', () => {
+//   readStream.close();
+// });
